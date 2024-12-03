2d3b4966c9726285d2caf7af187501a591d36b16
WillSchmidt97 <willschmidt1997@gmail.com>
2024-12-02 22:38:49 -0300

    added enum difficulty level

---
+++ NumberGuessingGame/GameLogic/DifficultyLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame.GameLogic
+{
+    public enum DifficultyLevel
+    {
+        Easy = 10,
+        Medium = 5,
+        Hard = 3
+    }
+}
--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace NumberGuessingGame
+﻿using NumberGuessingGame.GameLogic;
+
+namespace NumberGuessingGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to number guessing game!\n\n");
+            Console.WriteLine("Let's make it simple, I will think about a number from 1 to 100 and you will have to guess the number I chose.");
+            Console.WriteLine("We have three levels of difficulty: \n");
+
+            Console.WriteLine($"HARD: {DifficultyLevel.Easy} chances to guess the number\n" +
+                                $"MEDIUM: {DifficultyLevel.Medium} chances to guess the number\n" +
+                                $"HARD: {DifficultyLevel.Hard} chances to guess the number.");
+
+            Console.WriteLine("If you guess the number before you are out of tries, you win." +
+                                "But you ");
         }
     }
 }
