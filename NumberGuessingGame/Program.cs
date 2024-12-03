0ca326f0f25f84b1f0ba828b834671b62f7d6e7a
WillSchmidt97 <willschmidt1997@gmail.com>
2024-12-02 22:51:37 -0300

    chosing numbers

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -15,7 +15,21 @@ namespace NumberGuessingGame
                                 $"HARD: {DifficultyLevel.Hard} chances to guess the number.");
 
             Console.WriteLine("If you guess the number before you are out of tries, you win." +
-                                "But you ");
+                                "But you run your tries to zero, I win. So, let's get started?\n\n");
+
+            Console.WriteLine("Type 1 if you want you want it easier, 2 if you want to play the medium level" +
+                                "or 3 if you want more challenge: ");
+
+            int difficultyChosen = Convert.ToInt32(Console.ReadLine());
+
+            int difficultyLevel;
+
+            switch (difficultyChosen)
+            {
+                case 1: difficultyLevel = (int)DifficultyLevel.Easy; break;
+                case 2: difficultyLevel= (int)DifficultyLevel.Medium; break;
+                case 3: difficultyLevel=(int)DifficultyLevel.Hard; break;
+            }
         }
     }
 }
