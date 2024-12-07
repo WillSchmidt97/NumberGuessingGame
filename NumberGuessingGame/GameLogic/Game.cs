35b84521a852683202530f9b3f043a9e64c2f8bb
WillSchmidt97 <willschmidt1997@gmail.com>
2024-12-06 22:34:08 -0300

    another type of game mode added. Now the user can play countless times if doesn't want to choose a difficulty

--- NumberGuessingGame/GameLogic/Game.cs
+++ NumberGuessingGame/GameLogic/Game.cs
@@ -8,18 +8,37 @@ namespace NumberGuessingGame.GameLogic
 {
     internal class Game
     {
+        private int difficultyLevel;
         public void GameStart() 
         {
-            Console.WriteLine("Welcome to number guessing game!\n\n");
+            Console.WriteLine("Welcome to number guessing game!");
             Console.WriteLine("I'm thinking of a number between 1 and 100.");
-            Console.WriteLine("You have chances according to the difficulty chosen to guess the correct number.\n\n" +
-                                "Please select the difficult level");
 
-            int difficulty = GameDifficulty();
-            ProcessPlays(difficulty);
+            var gameMode = GameMode();
+            
+            if (gameMode == 1)
+            {
+                Console.WriteLine("You have chances according to the difficulty chosen to guess the correct number.\n\n" +
+                    "Please select the difficult level");
+
+                GameDifficulty();
+            }
+
+            ProcessPlays(gameMode);
         }
 
-        private int GameDifficulty()
+        public int GameMode()
+        {
+            Console.WriteLine("Yuou have two ways of playing the game:\n" +
+                            "1: You will have changes according to the difficulty chosen. When you get out of chances, the game finishes." +
+                            "2: You decide when to stop.\n\n" +
+                            "So, which way you want to play? (Please, type 1 or 2): ");
+            var gameMode = Int32.Parse(Console.ReadLine());
+
+            return gameMode;
+        }
+
+        private void GameDifficulty()
         {
             Console.WriteLine($"1. EASY: ({(int)DifficultyLevel.Easy} chances)\n" +
                     $"2. MEDIUM: ({(int)DifficultyLevel.Medium} chances)\n" +
@@ -29,19 +48,15 @@ namespace NumberGuessingGame.GameLogic
 
             int difficultyChosen = Convert.ToInt32(Console.ReadLine());
 
-            int difficultyLevel = 0;
-
             switch (difficultyChosen)
             {
-                case 1: difficultyLevel = (int)DifficultyLevel.Easy; break;
-                case 2: difficultyLevel = (int)DifficultyLevel.Medium; break;
-                case 3: difficultyLevel = (int)DifficultyLevel.Hard; break;
+                case 1: this.difficultyLevel = (int)DifficultyLevel.Easy; break;
+                case 2: this.difficultyLevel = (int)DifficultyLevel.Medium; break;
+                case 3: this.difficultyLevel = (int)DifficultyLevel.Hard; break;
             }
-
-            return difficultyLevel;
         }
 
-        private void ProcessPlays(int difficulty)
+        private void ProcessPlays(int gameMode)
         {
             Random random = new Random();
             int number = random.Next(1, 100);
@@ -50,7 +65,10 @@ namespace NumberGuessingGame.GameLogic
 
             int guessCounter = 1;
             bool isGuessed = false;
-            while (difficulty >= guessCounter)
+            var stop = false;
+            string choiceToStop = "";
+            while (gameMode == 1 ? this.difficultyLevel >= guessCounter
+                                    : stop == true)
             {
                 Console.WriteLine("Enter your guess: ");
                 int myGuess = Convert.ToInt32(Console.ReadLine());
@@ -66,11 +84,21 @@ namespace NumberGuessingGame.GameLogic
                     if (number > myGuess) Console.WriteLine($"Incorrect! The number is greater than {myGuess}.");
                     else Console.WriteLine($"Incorrect! The number is less than {myGuess}.");
 
+                    if (gameMode == 2)
+                    {
+                        Console.WriteLine("Do you want to stop? If so type Y/y: ");
+                        choiceToStop = Console.ReadLine().ToLower();
+
+                        if (choiceToStop == "Y") break;
+                    }
+
                     guessCounter++;
                 };
             }
 
             if (isGuessed == false) Console.WriteLine($"You are out of attempts! My number was {number}. Better luck next time!");
         }
+
+
     }
 }
