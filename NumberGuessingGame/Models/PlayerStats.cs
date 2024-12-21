3d11aac1658343a03d4a33260b1c86195db72b83
WillSchmidt97 <willschmidt1997@gmail.com>
2024-12-20 21:55:27 -0300

    created player stats model

---
+++ NumberGuessingGame/Models/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame.Models
+{
+    internal class PlayerStats
+    {
+        public string PlayerName { get; set; }
+        public string DifficultyLevel { get; set; }
+        public int Attempts { get; set; }
+        public DateTime DatePlayed { get; set; }
+    }
+}
